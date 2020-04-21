481ed2d67c5a4f38005be38efee11c54d4cf64d8
santiagop06 <santiagopuentes8@gmail.com>
2020-04-21 12:21:45 -0500

    Updates

---
+++ precache-manifest.cf19aac978eebb7fac4ad72ac2c5a9c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6206f4b50fa954368003e38560731e92",
+    "url": "/FaceApp/index.html"
+  },
+  {
+    "revision": "c712fd329d0e5a79fdc3",
+    "url": "/FaceApp/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "03abc847b72838e312ab",
+    "url": "/FaceApp/static/css/main.6469bc58.chunk.css"
+  },
+  {
+    "revision": "c712fd329d0e5a79fdc3",
+    "url": "/FaceApp/static/js/2.38de229a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceApp/static/js/2.38de229a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03abc847b72838e312ab",
+    "url": "/FaceApp/static/js/main.624a3b81.chunk.js"
+  },
+  {
+    "revision": "c9f3deabe67f60cd0334",
+    "url": "/FaceApp/static/js/runtime-main.994173f7.js"
+  }
+]);
